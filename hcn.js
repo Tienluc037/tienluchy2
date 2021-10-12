582b1497dc6ebebc2f7851aa5ebd52837dba1a63
unknown <nguyentienluc12a3@gmail.com>
2021-10-12 08:24:22 +0700

    dien tich HCN

---
+++ hcn.js
@@ -0,0 +1,6 @@
+let inputWidth = prompt("enter the width");
+let inputHeight = prompt("enter the Height");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("The area is: " + area);
